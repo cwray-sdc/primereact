4b6a7475637e559b25616d6f49d3f6b2a26bf609
Charles Wray <51002931+cwray-sdc@users.noreply.github.com>
2022-09-01 13:10:28 -0700

    Add custom property support to Split Button

--- components/lib/splitbutton/SplitButtonItem.js
+++ components/lib/splitbutton/SplitButtonItem.js
@@ -22,13 +22,13 @@ export const SplitButtonItem = React.memo((props) => {
         if (item.visible === false) {
             return null;
         }
-        const { disabled, icon: _icon, label: _label, template, url, target } = props.menuitem;
+        const { disabled, icon: _icon, label: _label, template, url, target, command, ...rest } = props.menuitem;        
         const className = classNames('p-menuitem-link', { 'p-disabled': disabled });
         const iconClassName = classNames('p-menuitem-icon', _icon);
         const icon = IconUtils.getJSXIcon(_icon, { className: 'p-menuitem-icon' }, { props: props.splitButtonProps });
         const label = _label && <span className="p-menuitem-text">{_label}</span>;
         let content = (
-            <a href={url || '#'} role="menuitem" className={className} target={target} onClick={onClick} aria-label={_label}>
+            <a href={url || '#'} role="menuitem" className={className} target={target} onClick={onClick} aria-label={_label} {...rest}>
                 {icon}
                 {label}
             </a>
